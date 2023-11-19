8e8836f4931fc170dd16864034d0645c580391dd
ElizandroGS <130264753+ElizandroGS@users.noreply.github.com>
2023-11-19 04:35:54 -0300

    Update and rename scriptx.sh to vpnproxy-ubuntu.sh

--- scriptx.sh
+++
@@ -1,4 +0,0 @@
-
-wget https://sourceforge.net/projects/webadmin/files/webmin/2.021/webmin-2.021-1.noarch.rpm && yum install webmin-2.021-1.noarch.rpm
-yum install -y wget && wget -O install.sh http://www.aapanel.com/script/install_6.0_en.sh && bash install.sh aapanel
-
