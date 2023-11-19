58ea1eb16dbfd7b047d6bc51556a56687a9d008c
ElizandroGS <130264753+ElizandroGS@users.noreply.github.com>
2023-11-19 05:03:27 -0300

    Update vpnproxy-ubuntu.sh

--- vpnproxy-ubuntu.sh
+++ vpnproxy-ubuntu.sh
@@ -1,3 +1,4 @@
+apt-get update && apt-get upgrade -y
 wget https://www.softether-download.com/files/softether/v4.43-9799-beta-2023.08.31-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.43-9799-beta-2023.08.31-linux-x64-64bit.tar.gz
 tar xvf softether-vpnserver-*.tar.gz
 sudo apt install gcc binutils gzip libreadline-dev libssl-dev libncurses5-dev libncursesw5-dev libpthread-stubs0-dev -y
