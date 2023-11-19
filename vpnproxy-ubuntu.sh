b447081662babd4d478c3b68be80c5d9ab7cac32
ElizandroGS <130264753+ElizandroGS@users.noreply.github.com>
2023-11-19 04:49:23 -0300

    Update vpnproxy-ubuntu.sh

--- vpnproxy-ubuntu.sh
+++ vpnproxy-ubuntu.sh
@@ -1,1 +1,18 @@
-
+wget https://www.softether-download.com/files/softether/v4.43-9799-beta-2023.08.31-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.43-9799-beta-2023.08.31-linux-x64-64bit.tar.gz
+tar xvf softether-vpnserver-*.tar.gz
+sudo apt install gcc binutils gzip libreadline-dev libssl-dev libncurses5-dev libncursesw5-dev libpthread-stubs0-dev -y
+sudo mv vpnserver /opt/softether
+cd vpnserver/
+make
+vpnserver start
+vpnserver stop
+sudo systemctl start softether-vpnserver
+sudo systemctl enable softether-vpnserver
+sudo journalctl -eu softether-vpnserver
+cd
+git clone https://github.com/thewhiteh4t/seeker.git
+apt-get install snap
+snap install ngrok -y
+git clone https://github.com/mauricionet/VPS-Manager-2.0.git
+git clone https://github.com/mauricionet/OpenVPNSetup.git
+apt-get install lamp-server^ -y
