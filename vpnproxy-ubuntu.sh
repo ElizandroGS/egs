e08de1814f814d033f5b2b0c15d4e638acba3531
ElizandroGS <130264753+ElizandroGS@users.noreply.github.com>
2023-11-19 10:39:37 -0300

    Update vpnproxy-ubuntu.sh

--- vpnproxy-ubuntu.sh
+++ vpnproxy-ubuntu.sh
@@ -27,9 +27,9 @@ systemctl start pure-ftpd
 adduser egs
 apt install openssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/selfsigned.key -out /etc/ssl/certs/selfsigned.crt
-echo"TLS=2
-CertFile /etc/ssl/certs/selfsigned.crt
-KeyFile /etc/ssl/private/selfsigned.key" >> /etc/pure-ftpd/pure-ftpd.conf
+"TLS=2"  >> /etc/pure-ftpd/pure-ftpd.conf
+"CertFile /etc/ssl/certs/selfsigned.crt"  >> /etc/pure-ftpd/pure-ftpd.conf
+"KeyFile /etc/ssl/private/selfsigned.key" >> /etc/pure-ftpd/pure-ftpd.conf
 systemctl restart pure-ftpd
 
 
