541cb9d580e6d2c51a2b22cbb9eabfa43d01002d
ElizandroGS <130264753+ElizandroGS@users.noreply.github.com>
2023-11-19 10:50:44 -0300

    Update vpnproxy-ubuntu.sh

--- vpnproxy-ubuntu.sh
+++ vpnproxy-ubuntu.sh
@@ -28,5 +28,6 @@ adduser egs
 apt install openssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/selfsigned.key -out /etc/ssl/certs/selfsigned.crt
 systemctl restart pure-ftpd
+apt-get install softether-vpnserver -y
 
 
